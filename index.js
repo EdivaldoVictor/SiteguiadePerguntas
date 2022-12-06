6c59fcecf13fa3df069b438b90ff0f4094eba8ec
Victor <haillothere@gmail.com>
2022-12-06 20:07:39 -0300

    Commitando body-parser e Sequelize

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //Estou dizendo ao express para usar o EJS como view engine
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Rotas
 app.get("/",(req,res ) =>{
 
     res.render("index",{
@@ -16,7 +20,9 @@ app.get("/perguntar",(req,res) =>{
 });
 
 app.post("/salvarpergunta", (req,res) =>{
-    res.send("Formulário recebido!")
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    res.send("Formulário recebido ! titulo: " + " " + titulo + " descricao " + descricao); 
 });
 
 app.listen(8080,() =>{console.log("Listening on port 8080");});
