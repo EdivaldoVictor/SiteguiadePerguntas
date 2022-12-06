53885182b69e5d88d465d29464234663855d5165
Victor <haillothere@gmail.com>
2022-12-06 18:07:03 -0300

    Commitando mudanças do bootstrap

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+//Estou dizendo ao express para usar o EJS como view engine
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get("/",(req,res ) =>{
+
+    res.render("index",{
+    });
+});
+
+app.get("/perguntar",(req,res) =>{
+    res.render("perguntar")
+});
+
+app.listen(8080,() =>{console.log("Listening on port 8080");});
